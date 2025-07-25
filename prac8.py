4e57dd81454c7a7bc6737b2dbd98ea63a047274f
Guru-24 <guru.g@s.amity.edu>
2025-07-26 00:10:31 +0530

    created a pyhon program for number guessing

---
+++ prac8.py
@@ -0,0 +1,8 @@
+import random
+
+def welcome():
+    print("====================================")
+    print("     Welcome to the Guess Game!")
+    print(" Try to guess the number (1 to 100)")
+    print("====================================")
+
