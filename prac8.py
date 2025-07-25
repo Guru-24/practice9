649f467efe695f745f56ade05d1f1f911996e8aa
Guru-24 <guru.g@s.amity.edu>
2025-07-26 00:11:05 +0530

    deined a play function

--- prac8.py
+++ prac8.py
@@ -5,4 +5,9 @@ def welcome():
     print("     Welcome to the Guess Game!")
     print(" Try to guess the number (1 to 100)")
     print("====================================")
+def play_game():
+    number = random.randint(1, 100)
+    attempts = 0
+    max_attempts = 10
+    score = 100
 
