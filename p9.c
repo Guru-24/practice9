70143be7fd81231a3d16a4322c07ad1feae4578f
Guru-24 <guru.g@s.amity.edu>
2025-07-26 23:20:05 +0530

    printing the result

--- p9.c
+++ p9.c
@@ -22,4 +22,15 @@ for (i = 0; i < r; ++i)
     for (j = 0; j < c; ++j) {
       sum[i][j] = a[i][j] + b[i][j];
     }
+ printf("\nSum of two matrices: \n");
+  for (i = 0; i < r; ++i)
+    for (j = 0; j < c; ++j) {
+      printf("%d   ", sum[i][j]);
+      if (j == c - 1) {
+        printf("\n\n");
+      }
+    }
+
+  return 0;
+}
 
